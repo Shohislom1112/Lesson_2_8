9c5ac8fea25bbc19261aa9123468312d5b9dc479
Shohislom1112 <ravshanovshohislom11@gmail.com>
2024-02-24 08:48:56 +0500

    Asdas

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,75 +1,69 @@
 // import React from 'react'
-import { useState, useEffect } from 'react';
-import './Todos.scss'
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import "./Todos.scss";
+import axios from "axios";
 
 const Todos = () => {
-    const [loading, setLoading] = useState<boolean>(false);
-const [todos, setTodos] = useState<any[]>([
-    {id: Number,
-    title: String,
-    complete: Boolean,}
-]);
-//  loading any;
-// const [error, setError] = useState<Error | null>(null);
-// const [openAdd, setOpenAdd] = useState<boolean>(false);
-// const [openEdit, setOpenEdit] = useState<boolean>(false);
-// const [editedTodo, setEditedTodo] = useState<todos | <null>(null);
-// const [deleteTodo, setDeleteTodo] = useState<todos | <null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [todos, setTodos] = useState<any[]>([
+    // {/
+    //   id: Number, title: String, complete: Boolean
+    // },
+  ]);
+  const [edit, setEdit] = useState<string>("");
+//   const [openEdit, setOpenEdit] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
 
+  const fetchTodos = async (): Promise<void> => {
+    setLoading(true);
+    try {
+      const res = await axios.get<any[]>("http://localhost:3000/Todos");
+      const data: any[] = res.data;
+      setTodos(data);
+    } finally {
+      // catch (error) {
+      //   setError(error);
+      // }
+      setLoading(false);
+    }
+  };
+  const handleEdit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEdit(e.target.value);
+  };
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
 
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
-
-
-
-    const fetchTodos = async (): Promise<void> => {
-      
-        setLoading(true);
-        try {
-          const res = await axios.get<any[]>("http://localhost:3000/Todos");
-          const data: any[] = res.data;
-          setTodos(data);
-        } 
-        // catch (error) {
-        //   setError(error);
-        // } 
-        finally {
-          setLoading(false);
-        }
-      };
-      useEffect(() => {
-        fetchTodos();
-      }, []);
-      
-
-
-      
   return (
-    <div className='Todos'>
-            <p className="p">Welcome to Todos</p>
-        <div className="container">
-                    {todos.map((td:any) => (
-            <div className="cards">
-                <div className="card1" key={td.id}>
-                    <p className="p1">  Todo-{td.id} : {td.title}  </p>
-                    <p className="p2">{td.complate ? '✔' : '❌'}</p>
-                    
-
-                </div>
-                    <div className="card2">
-                        <button className="button1"
-                      
-                        >Edit</button>
-                        <button className="button2" >Delete</button>
-                </div>
-                
+    <div className="Todos">
+      <p className="p">Welcome to Todos</p>
+      <div className="container">
+        {todos.map((td: any) => (
+          <div className="cards" key={td.id}>
+            <div className="card1">
+              <p className="p1">
+                {" "}
+                Todo-{td.id} : {td.title}
+              </p>
+              <p className="p2">{td.completed ? "✔" : "❌"}</p>
             </div>
-                    )
-                    )}
-        </div>
-
+            <div className="card2">
+              <button className="button1" onClick={handleDoubleClick} >Edit</button>
+              <button className="button2">Delete</button>
+            </div>
+            <div className="input" onDoubleClick={handleDoubleClick} >
+              <input type="text" id="text" value="text" onChange={handleEdit}  className="text" />
+              
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todos
+export default Todos;
